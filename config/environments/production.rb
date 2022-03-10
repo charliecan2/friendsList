734251621a5fbbad3473fa8e8854da9725f4b364
Carlos Cantu <carloscq920@gmail.com>
2022-03-10 12:39:00 -0600

    Changed the homepage styling

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.assets.precompile += %w( '.svg' )
+  config.assets.css_compressor = :sass
 end
